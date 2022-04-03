7fe34c7b47a30e689371e4449768b5e0ea6a97e4
DAVIDE SAVARRO <20023866@studenti.uniupo.it>
2022-04-03 23:26:37 +0200

    Implementing Kmeans distance

--- Clustering/kmeans.py
+++ Clustering/kmeans.py
@@ -1,20 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 3 15:15:00 2022
 
-@author: Davide Savarro
-"""
+import random
 
-
-import numpy as np
-import pandas as pd
-
-dataset = pd.read_csv("data/Data_for_UCI_named.csv")
-# The p1 attribute is a non predictive one 
-dataset = dataset.drop('p1', axis=1)
-
-# I create two datasets where there are two different target attribute
-datasetStab = dataset.drop('stabf', axis=1)
-datasetStabF = dataset.drop('stab', axis=1)
-
-print(datasetStabF)
+class Kmeans:
+    def __init__(self, dataset, k):
+        self.dataset = dataset
+        self.k = k
+        
+    def clusterize(self):
+        centroids = self.initCentroids()
+        
+        
+    def initCentroids(self):
+        centroids = []
+        for i in 0..self.k:
+            index = random.randint(0, self.dataset.count(axis=0)[0])
+            centroids.append(self.dataset.iloc[index])
+        return centroids
+    
+    def calculateDistance(self, a, b):
+        lenght = self.dataset.count(axis=0)[0]
+        #for key in 0..lenght:
+            
