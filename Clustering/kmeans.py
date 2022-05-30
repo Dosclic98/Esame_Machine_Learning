c2ef94f4294e4b9ca9939b3a4fbfbbbfdbc3900a
DAVIDE SAVARRO <20023866@studenti.uniupo.it>
2022-05-31 00:08:58 +0200

    Almost done K-means implementation

--- Clustering/kmeans.py
+++ Clustering/kmeans.py
@@ -1,23 +1,47 @@
 
+import math
+from multiprocessing.dummy import Array
+from cv2 import sqrt
+import numpy as np
 import random
 
 class Kmeans:
     def __init__(self, dataset, k):
-        self.dataset = dataset
+        # Remove the last column (the target one)
+        self.dataset = dataset.iloc[: , :-1]
+        # Save the target row in a separate variable
+        self.target = dataset.iloc[: , -1]
         self.k = k
+        self.clusters = [0] * self.dataset.shape[0]
         
     def clusterize(self):
         centroids = self.initCentroids()
+        quiescent = False
+        while quiescent == False:
+            oldClusters = self.clusters
+            for index, row in zip(range(0,self.dataset.shape[0]), self.dataset.itertuples()):
+                distances = []
+                for center in centroids:
+                    distances.append(self.calculateDistance(center, row))
+                self.clusters[index] = distances.index(min(distances))
+            self.recalculateCentroids()
+            quiescent = np.array_equal(self.clusters, oldClusters)
         
         
     def initCentroids(self):
         centroids = []
-        for i in 0..self.k:
+        for i in range(0, self.k):
             index = random.randint(0, self.dataset.count(axis=0)[0])
             centroids.append(self.dataset.iloc[index])
         return centroids
     
+    def recalculateCentroids(self):
+        # TODO From here
+    
     def calculateDistance(self, a, b):
-        lenght = self.dataset.count(axis=0)[0]
-        #for key in 0..lenght:
+        lenght = len(a)
+        sum = 0
+        for key in range(0, lenght):
+            sum += pow(a[key] - b[key], 2)
+        return math.sqrt(sum)
             
--- Clustering/main.py
+++ Clustering/main.py
@@ -9,6 +9,8 @@ Created on Sun Apr 3 15:15:00 2022
 import numpy as np
 import pandas as pd
 
+from kmeans import Kmeans
+
 dataset = pd.read_csv("data/Data_for_UCI_named.csv")
 # The p1 attribute is a non predictive one 
 dataset = dataset.drop('p1', axis=1)
@@ -17,6 +19,5 @@ dataset = dataset.drop('p1', axis=1)
 datasetStab = dataset.drop('stabf', axis=1)
 datasetStabF = dataset.drop('stab', axis=1)
 
-datasetStab.size
-
-print(datasetStab.columns)
+cl1 = Kmeans(datasetStab, 3)
+cl1.clusterize()
