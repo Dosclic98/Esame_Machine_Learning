f027d08ec164853802b10c5ec2549e77f5b26c53
DAVIDE SAVARRO <20023866@studenti.uniupo.it>
2022-06-10 02:02:05 +0200

    Clustering done but to test, still to do confusion matrix

--- Clustering/kmeans.py
+++ Clustering/kmeans.py
@@ -1,9 +1,11 @@
 
 import math
 from multiprocessing.dummy import Array
+from operator import index
 from cv2 import sqrt
 import numpy as np
 import random
+from collections import Counter
 
 class Kmeans:
     def __init__(self, dataset, k):
@@ -14,24 +16,30 @@ class Kmeans:
         self.k = k
         self.clusters = [0] * self.dataset.shape[0]
         
-    def clusterize(self):
+    def clusterize(self, showConfusion = False):
         centroids = self.initCentroids()
         quiescent = False
         while quiescent == False:
+            print('QUI')
             oldClusters = self.clusters
-            for index, row in zip(range(0,self.dataset.shape[0]), self.dataset.itertuples()):
+            for index, row in zip(range(0,self.dataset.shape[0]), self.dataset.itertuples(index=False)):
                 distances = []
                 for center in centroids:
                     # If the centroid representing that group is set to None it means there was 
                     # no element part of that group
                     if(center is not None):
+                        #print('YELLO')
                         distances.append(self.calculateDistance(center, row))
                     else:
                         distances.append(None)
                 self.clusters[index] = distances.index(min(dist for dist in distances if dist is not None))
             oldCentroids = centroids.copy()
-            self.recalculateCentroids()
-            quiescent = np.array_equal(self.clusters, oldClusters)
+            centroids = self.recalculateCentroids()
+            quiescent = np.array_equal(centroids, oldCentroids)
+        if(showConfusion):
+            # TODO Implement confusion matrix
+            print(Counter(self.target))
+            print(np.bincount(self.clusters))
         
         
     def initCentroids(self):
@@ -44,15 +52,17 @@ class Kmeans:
     def recalculateCentroids(self):
         centroids = [None] * self.k
         numElem = [0] * self.k
-        for cl, row in zip(self.clusters, self.dataset.itertuples()):
-            if(centroids[cl] == None): centroids[cl] = row
-            else: centroids[cl] += row
+        for cl, row in zip(self.clusters, self.dataset.itertuples(index=False)):
+            if(centroids[cl] == None): 
+                centroids[cl] = row
+            else: 
+                for center, rowEl in zip(centroids[cl], row): center += rowEl
             numElem[cl] += 1
-        # TODO Fix his division
-        centroids[cl] = centroids[cl] / numElem[cl]
+        for center in centroids[cl]: center = center / numElem[cl]
         return centroids
     
     def calculateDistance(self, a, b):
+        assert(len(a) == len(b))
         lenght = len(a)
         sum = 0
         for key in range(0, lenght):
