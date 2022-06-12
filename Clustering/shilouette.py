31c7a3ed6c3018ce5994b12cd9edc5b38fc598d1
DAVIDE SAVARRO <20023866@studenti.uniupo.it>
2022-06-13 00:56:05 +0200

    STUB

--- Clustering/shilouette.py
+++ Clustering/shilouette.py
@@ -68,7 +68,15 @@ for n_clusters in range_n_clusters:
 
         color = cm.nipy_spectral(float(i) / n_clusters)
         
-        
+        ax1.fill_betweenx(
+            np.arange(y_lower, y_upper),
+            0,
+            ith_cluster_silhouette_values,
+            facecolor=color,
+            edgecolor=color,
+            alpha=0.7,
+        )
+
         # Label the silhouette plots with their cluster numbers at the middle
         ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
 
