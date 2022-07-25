40ad973d8596950d002eb175da266fe9aee9335c
DAVIDE SAVARRO <20023866@studenti.uniupo.it>
2022-07-26 00:59:32 +0200

    Almost done clustering validation

--- Clustering/kmeans.py
+++ Clustering/kmeans.py
@@ -1,4 +1,5 @@
 
+from ast import And
 import math
 from multiprocessing.dummy import Array
 from operator import index
@@ -7,6 +8,7 @@ import numpy as np
 import pandas as pd
 import random
 from collections import Counter
+import sklearn
 from sklearn.metrics import silhouette_samples, silhouette_score
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
@@ -21,15 +23,16 @@ class Kmeans:
         self.target = dataset.iloc[: , -1]
         self.k = k
         self.clusters = [0] * self.dataset.shape[0]
+        self.centroids = []
         
-    def clusterize(self, showConfusion = False, showSilhouette = False):
-        centroids = self.initCentroids()
+    def clusterize(self):
+        self.centroids = self.initCentroids()
         quiescent = False
         while quiescent == False:
             oldClusters = self.clusters
             for index, row in zip(range(0,self.dataset.shape[0]), self.dataset.itertuples(index=False)):
                 distances = []
-                for center in centroids:
+                for center in self.centroids:
                     # If the centroid representing that group is set to None it means there was 
                     # no element part of that group
                     if(center is not None):
@@ -37,25 +40,20 @@ class Kmeans:
                     else:
                         distances.append(None)
                 self.clusters[index] = distances.index(min(dist for dist in distances if dist is not None))
-            oldCentroids = centroids.copy()
-            centroids = self.recalculateCentroids()
-            quiescent = np.array_equal(centroids, oldCentroids)
-        if(showConfusion):
-            print(Counter(self.target))
-            print(np.bincount(self.clusters))
-            df = pd.DataFrame({'Labels': self.target, 'Clusters': self.clusters})
-            ct = pd.crosstab(df['Labels'], df['Clusters'])
-            print(ct)
-        if(showSilhouette):
-            self.calcPlotSilhouete()
+            oldCentroids = self.centroids.copy()
+            self.centroids = self.recalculateCentroids()
+            quiescent = np.array_equal(self.centroids, oldCentroids)
+            
+        cost = self.optimizFunc()
+        print("Cost is:", cost)
 
-        return self.clusters
+        return self.clusters, cost
         
     def initCentroids(self):
         centroids = []
         random.seed = 10
         for i in range(0, self.k):
-            index = random.randint(0, self.dataset.count(axis=0)[0])
+            index = random.randint(0, self.dataset.count(axis=0)[0]-1)
             centroids.append(self.dataset.iloc[index])
         return centroids
     
@@ -79,6 +77,13 @@ class Kmeans:
             sum += pow(a[key] - b[key], 2)
         return math.sqrt(sum)
     
+    def optimizFunc(self):
+        n = self.dataset.count(axis=0)[0]
+        acc = 0
+        for index, row in zip(range(0,self.dataset.shape[0]), self.dataset.itertuples(index=False)):
+            acc += self.calculateDistance(row, self.centroids[self.clusters[index]])
+        return acc / n
+    
     def calcPlotSilhouete(self):
         # Create a subplot with 1 row and 2 columns
         fig, (ax1) = plt.subplots(1, 1)
@@ -92,19 +97,23 @@ class Kmeans:
         # plots of individual clusters, to demarcate them clearly.
         ax1.set_ylim([0, len(self.dataset) + (self.k + 1) * 10])
         
+        # Compute the silhouette scores for each sample
+        sample_silhouette_values = []
+        if self.dataset.shape[0] > 200:
+            sample_silhouette_values = silhouette_samples(self.dataset, self.clusters)
+        else:
+            sample_silhouette_values = self.silhouetteCoeff()
+        
         # The silhouette_score gives the average value for all the samples.
         # This gives a perspective into the density and separation of the formed
         # clusters
-        silhouette_avg = silhouette_score(self.dataset, self.clusters)
+        silhouette_avg = self.meanSilhouette(sample_silhouette_values)
         print(
             "For k clusters =",
             self.k,
             "The average silhouette_score is :",
             silhouette_avg,
         )
-        
-        # Compute the silhouette scores for each sample
-        sample_silhouette_values = silhouette_samples(self.dataset, self.clusters)
         
         y_lower = 10
         for i in range(self.k):
@@ -145,4 +154,103 @@ class Kmeans:
 
         ax1.set_yticks([])  # Clear the yaxis labels / ticks
         ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+        plt.show()
+        
+    def silhouetteCoeff(self):
+        silhouettes = np.array([]) 
+        for i in range(self.dataset.shape[0]):
+            a = self.silhCoeffA(i)
+            b = self.silhCoeffB(i)
+            if a < b:
+                silhouettes = np.append(silhouettes, 1-(a/b))
+            elif a == b:
+                silhouettes = np.append(silhouettes, 0)
+            else:
+                silhouettes = np.append(silhouettes, (b/a)-1)
+        return silhouettes
+        
+    def silhCoeffA(self, index):
+        count = 0
+        sum = 0
+        for j in range(self.dataset.shape[0]):
+            if index != j and self.clusters[index] == self.clusters[j]:
+                sum += self.calculateDistance(self.dataset.iloc[index], self.dataset.iloc[j])
+                count += 1
+        return sum / (count - 1)
+
+    def silhCoeffB(self, index):
+        min = float("inf")
+        for cl in range(self.k):
+            if cl != self.clusters[index]:
+                count = 0
+                sum = 0
+                tmp = 0
+                for j in range(self.dataset.shape[0]):
+                    if index != j and self.clusters[j] == cl:
+                        sum += self.calculateDistance(self.dataset.iloc[index], self.dataset.iloc[j])
+                        count += 1
+                tmp = sum / count
+                if tmp < min:
+                    min = tmp
+        return min
+
+    def meanSilhouette(self, silhouettes):
+        sum = 0
+        for elem in silhouettes:
+            sum += elem
+        return sum / len(silhouettes)
             
+    def calcExtValidation(self):
+        print(Counter(self.target))
+        print(np.bincount(self.clusters))
+        df = pd.DataFrame({'Labels': self.target, 'Clusters': self.clusters})
+        ct = pd.crosstab(df['Labels'], df['Clusters'])
+        print(ct)
+        self.assignClusters(ct)
+        self.calcPrecRecall()
+        
+    def assignClusters(self, confusionMatrix):
+        wrongClustered = 0
+        sum = 0
+        for i in range(confusionMatrix.shape[0]):
+            clusterNum = list(confusionMatrix.iloc[i][confusionMatrix.iloc[i] == max(confusionMatrix.iloc[i])].items())[0][0]
+            clusterLabel = confusionMatrix.head().index[i]
+            print("Cluster",clusterNum,":",clusterLabel)
+            for j in range(len(confusionMatrix.iloc[i])):
+                if(j != clusterNum):
+                    wrongClustered += confusionMatrix.iloc[i][j]
+                else:
+                    sum += confusionMatrix.iloc[i][j]
+        print("\nIncorrectly clustered instances:", wrongClustered)
+        print("Purity:", sum / self.dataset.shape[0])
+    
+    def calcPrecRecall(self):
+        numCols = self.dataset.shape[1]
+        tpSum = 0
+        tnSum = 0
+        fpSum = 0
+        fnSum = 0
+        for i in range(self.dataset.shape[0]):
+            for j in range(i+1, self.dataset.shape[0]):
+                if self.dataset.iloc[i][numCols-1] == self.dataset.iloc[i][numCols-1] and self.clusters[i] == self.clusters[j]:
+                    tpSum = tpSum + 1
+                elif self.dataset.iloc[i][numCols-1] != self.dataset.iloc[i][numCols-1] and self.clusters[i] != self.clusters[j]:
+                    tnSum = tnSum + 1
+                elif self.dataset.iloc[i][numCols-1] != self.dataset.iloc[i][numCols-1] and self.clusters[i] == self.clusters[j]:
+                    fpSum = fpSum + 1
+                else:
+                    fnSum = fnSum + 1
+        den = math.comb(self.dataset.shape[0], 2)
+        TP = tpSum / den
+        TN = tnSum / den
+        FP = fpSum / den
+        FN = fnSum / den
+        rand_index = (TP+TN)/(TP+TN+FP+FN)
+        precision = TP/(TP+FP)
+        recall = TP/(TP+FN)
+        f1measure = (2*(precision*recall)) / (precision+recall)
+        print("Accuracy:", rand_index)
+        print("Precision:", precision)
+        print("Recall:", recall)
+        print("F score:", f1measure)
+        
--- Clustering/main.py
+++ Clustering/main.py
@@ -5,11 +5,39 @@ Created on Sun Apr 3 15:15:00 2022
 @author: Davide Savarro
 """
 
+from calendar import c
 import numpy as np
 import pandas as pd
-import os
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
 
 from kmeans import Kmeans
+
+def elbowMethod(dataset, maxClusters, maxIterations):
+    costs = []
+    for nCen in range(1, maxClusters+1):
+        print("Calculating elbow for kClusters:", nCen)
+        clustering, cost = findBestClusering(dataset, nCen, maxIterations)
+        costs.append(cost)
+    plt.plot(np.arange(1, maxClusters+1), np.array(costs))
+    plt.xlabel("K clusters")
+    plt.ylabel("Cost function")
+    plt.title("Elbow method")
+    plt.show()
+    
+
+def findBestClusering(dataset, kClusters, maxIterations):
+    bestClustering = []
+    bestCost = float("inf")
+    for i in range(maxIterations):
+        cl = Kmeans(dataset, kClusters)
+        clusters, currCost = cl.clusterize()
+        if currCost < bestCost:
+            bestCost = currCost
+            bestClustering = cl
+    return bestClustering, bestCost
+
 
 dataset = pd.read_csv("data/Data_for_UCI_named.csv")
 # The p1 attribute is a non predictive one 
@@ -19,12 +47,16 @@ dataset = dataset.drop('p1', axis=1)
 datasetStab = dataset.drop('stabf', axis=1)
 datasetStabF = dataset.drop('stab', axis=1)
 
-#cl1 = Kmeans(datasetStab, 2)
-#cl1.clusterize(showConfusion=False)
-
-cl2 = Kmeans(datasetStabF, 2)
-cl2.clusterize(showConfusion=True, showSilhouette=False)
+cl, cost = findBestClusering(datasetStabF, 2, 5)
+cl.calcExtValidation()
+cl.calcPlotSilhouete()
 
 dataset = pd.read_csv("data/iris.csv")
-cl3 = Kmeans(dataset, 3)
-cl3 = cl3.clusterize(showConfusion=True, showSilhouette=True)
+cl, cost = findBestClusering(dataset, 3, 10)
+cl.calcExtValidation()
+cl.calcPlotSilhouete()
+
+
+elbowMethod(datasetStabF, 5, 5)
+elbowMethod(dataset, 6, 10)
+
