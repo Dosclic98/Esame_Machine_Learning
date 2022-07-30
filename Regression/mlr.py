2201e34da30ae152d5429b95ef3984c21e6a704f
DAVIDE SAVARRO <20023866@studenti.uniupo.it>
2022-07-30 11:50:19 +0200

    Cv implemented, to add averagig over cv results

--- Regression/mlr.py
+++ Regression/mlr.py
@@ -18,12 +18,13 @@ class MLR:
         self.target = dataset[[targetVarName]]
         self.dataset = dataset.drop(targetVarName, axis=1)
         # Normalize with one of the following methods
-        if normMethod == "AUTO":
+        self.normMethod = normMethod
+        if self.normMethod == "AUTO":
             self.dataset = self.dataset.apply(lambda x: (x - x.mean()) / (x.std()))
-        elif normMethod == "MINMAX":
+        elif self.normMethod == "MINMAX":
             self.dataset = self.dataset.apply(lambda x: (x - x.mean()) / (x.max() - x.min()))
         else:
-            raise Exception("Unable to dine normalization method:", normMethod)
+            raise Exception("Unable to find normalization method:", normMethod)
         # Adding Bias
         self.dataset.insert(0, 'x0', np.ones(self.dataset.shape[0]), True)
     
@@ -46,7 +47,18 @@ class MLR:
 
         return pred, self.params
     
-    # TODO Implement method to predict on a subset of data
+    def test(self, x):
+        # Normalizng
+        if self.normMethod == "AUTO":
+            x = x.apply(lambda x: (x - x.mean()) / (x.std()))
+        elif self.normMethod == "MINMAX":
+            x = x.apply(lambda x: (x - x.mean()) / (x.max() - x.min()))
+        else:
+            raise Exception("Unable to find normalization method:", self.normMethod)
+        # Adding Bias
+        x.insert(0, 'x0', np.ones(x.shape[0]), True)
+        return self.predict(x, self.params)
+        
         
     
     def predict(self, x, params):
